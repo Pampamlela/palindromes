9414101266cc7d35533e2c79a5fb556636f0a169
Pampamlela <pampamlela@gmail.com>
2025-03-11 15:35:46 +0100

    début

---
+++ script.js
@@ -0,0 +1,19 @@
+/*Étape 1
+
+Créer une fonction isValidDate qui prend en paramètre une date en string 
+et determine si elle est valide. Pour qu'une date soit valide, 
+il faut qu'elle existe et qu'elle soit au format dd/mm/aaaa.
+
+Tout au long de l’exercice, on supposera des années supérieures à 999 et inférieures 9999 
+- autrement dit, l’année sera systématiquement représentée sur 4 caractères.
+
+isValidDate("03/04/2001") // true
+isValidDate("03/14/2001") // false car 14 n'est pas un mois valide
+
+Vous aurez probablement besoin de créer une autre fonction maxDaysInMonth 
+pour vous assurer que le nombre de jours par mois est valide 
+(ex: le 31/11 n’est pas valide, le mois de novembre ne contient que 30 jours)*/
+
+function isValidDate(date){
+    
+}
